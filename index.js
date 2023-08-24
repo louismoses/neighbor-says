e058a7aee8fc73988f39f582d4d026f314479f5e
Louis Moses <louismoses.rait@gmail.com>
2023-08-24 08:28:57 +0800

    initial configurations

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get("https://www.affirmations.dev/");
+    res.render("index.ejs", { affirm: result.data.affirmation });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server is running at port ${port}`);
+});
